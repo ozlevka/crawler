ed3ff5a7ba1b51461b9d512a09b29c7c4b0cfdf8
Lev <ozlevka@gmail.com>
2013-09-21 18:44:38 +0300

    MonogDB common test added

--- CrawlerTest/CrawlerTest/DALCommonTests.cs
+++ CrawlerTest/CrawlerTest/DALCommonTests.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using System.Linq;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace CrawlerTest
 {
@@ -7,8 +12,41 @@ namespace CrawlerTest
     public class DALCommonTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestConnectionCRUD()
         {
+            MongoClient client = new MongoClient();
+            MongoServer server = client.GetServer();
+
+            MongoDatabase db = server.GetDatabase("testcrud");
+
+            string actualName = Guid.NewGuid().ToString();
+
+            db.CreateCollection("testcollection");
+
+            MongoCollection<TestObject> collection = db.GetCollection<TestObject>("testcollection");
+
+            TestObject obj = new TestObject
+            {
+                Name = actualName
+            };
+
+            collection.Insert(obj);
+            TestObject tst = collection.AsQueryable<TestObject>().Single<TestObject>(e => e.Name == actualName);
+
+            Assert.IsNotNull(tst);
+            
+
+            server.DropDatabase("testcrud");
         }
+
+
+    }
+
+    public class TestObject
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        
+        public string Name { get; set; }
     }
 }
