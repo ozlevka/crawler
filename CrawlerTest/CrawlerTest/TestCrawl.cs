25c28056a7b1053b1f15d8bed228f0013a79b735
Lev <ozlevka@gmail.com>
2013-12-12 21:42:38 +0200

    Add new test for agility pack testing

---
+++ CrawlerTest/CrawlerTest/TestCrawl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrawlerTest
+{
+    [TestClass]
+    public class TestCrawl
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
