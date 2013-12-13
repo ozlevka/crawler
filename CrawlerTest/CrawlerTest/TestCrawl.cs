714d54d4200df910431fa5d874bdb3d88a880b07
Lev <ozlevka@gmail.com>
2013-12-13 11:28:10 +0200

    Switch to using Configuration + Model + CsQuery

--- CrawlerTest/CrawlerTest/TestCrawl.cs
+++ CrawlerTest/CrawlerTest/TestCrawl.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CsQuery;
 
 namespace CrawlerTest
 {
@@ -9,7 +10,13 @@ namespace CrawlerTest
         [TestMethod]
         public void TestParsing()
         {
-                        
+            CQ dom = "<div>Hello world! <b>I am feeling bold!</b> What about <b>you?</b></div>";
+
+            var res = dom.Select("div b");
+
+            Assert.AreEqual(res.Length, 2);
+            Assert.AreEqual(res[0].TextContent, "I am feeling bold!");
+            Assert.AreEqual(res[1].TextContent, "you?");
         }
     }
 }
