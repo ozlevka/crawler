140c3a6e4623416c9f224e92db06a020d978d24c
Lev <ozlevka@gmail.com>
2013-12-14 19:48:02 +0200

    Add New RSS test

---
+++ CrawlerTest/CrawlerTest/RSSFeedsTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml;
+using System.ServiceModel.Syndication;
+using System.Diagnostics;
+
+namespace CrawlerTest
+{
+    [TestClass]
+    public class RSSFeedsTests
+    {
+        [TestMethod]
+        public void TestSyndicationFeed()
+        {
+            SyndicationFeed feed; 
+            using(XmlReader reader = XmlReader.Create("http://feeds.feedburner.com/lccLatestDeals"))
+            {
+                feed = SyndicationFeed.Load(reader);
+            }
+
+            foreach (var item in feed.Items)
+            {
+                Debug.Print(string.Format("title:{0} Summary: {1}", item.Title.Text, item.Summary.Text));
+            }
+        }
+    }
+}
